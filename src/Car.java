6d7323bbabbaadc13bad7c28ed3c609540b147b9
dbwofla11 <mynok2714@gmail.com>
2023-04-06 16:37:01 +0900

    Week3

---
+++ src/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    private String name;
+    private int speed;
+
+
+    public Car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getSpeed(){
+        return this.speed;
+    }
+
+}
