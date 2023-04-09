f931800ecb96a1c1c7edee9d02a2944854029cdc
2zzzi <2ghun1210@gmail.com>
2023-04-09 18:19:11 +0900

    fix: 3주차 과제 카멜케이스 적용

--- src/CarRacing/CarTest.java
+++ src/CarRacing/CarTest.java
@@ -10,18 +10,18 @@ public class CarTest {
 
         Scanner scanner = new Scanner(System.in);
         int carCount = scanner.nextInt();
-        int Speed;
-        String Name;
+        int speed;
+        String name;
 
         ArrayList<Car> carArrayList = new ArrayList<>();
 
         for (int i = 1; i <= carCount; i++) {
             System.out.println(i + "번째 자동차의 스피드를 입력하세요.");
-            Speed = scanner.nextInt();
+            speed = scanner.nextInt();
             System.out.println(i + "번째 자동차의 이름을 입력하세요.");
-            Name = scanner.next();
+            name = scanner.next();
 
-            carArrayList.add(new Car(i, Speed, Name));
+            carArrayList.add(new Car(i, speed, name));
         }
 
         System.out.println("-----경기 참가자 소개-----");
