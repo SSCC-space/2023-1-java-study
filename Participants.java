af3067958fe4d83ebf78fc3d8dbb88258c4733b3
Yeonnnny <128607964+Yeonnnny@users.noreply.github.com>
2023-04-11 17:38:51 +0900

    수정했습니다!

--- Participants.java
+++ Participants.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-class Member{
+class Car{
     int speed;
     String name;
 
-    public Member(){}
+    public Car(){}
 
 
 
@@ -17,22 +17,21 @@ public class Participants {
         System.out.print("참가자 수 입력 (2명 이상): ");
         int num = scanner.nextInt();
 
-        Member[] m = new Member[num];
+        Car[] c = new Car[num];
 
         for (int i = 0; i < num; i++) {
-            m[i] = new Member();
-        }
-
-        for (int i = 0; i < num; i++) {
+            
+            c[i] = new Car();
+            
             System.out.print(i+1+"번째 참가자의 스피드를 입력하세요.\n");
-            m[i].speed = scanner.nextInt();
+            c[i].speed = scanner.nextInt();
             System.out.print(i+1+"번째 참가자의 이름을 입력하세요.\n");
-            m[i].name = scanner.next();
+            c[i].name = scanner.next();
             System.out.print("\n");
         }
         System.out.print("-----경기 참가자 소개-----\n");
         for (int i = 0; i < num; i++) {
-            System.out.print("스피드는 "+m[i].speed+"이고, 이름은 "+m[i].name+"입니다.\n");
+            System.out.print("스피드는 "+c[i].speed+"이고, 이름은 "+c[i].name+"입니다.\n");
         }
 
 
